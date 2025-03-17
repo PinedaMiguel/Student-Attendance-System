0cb322d831e14e43577f8ab45f0edb1640c50313
Miguel Angel <68487934+Mundzuk-Uldin@users.noreply.github.com>
2025-03-17 16:16:26 -0500

    Configure deploy and update README

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    cors: true, // Enable CORS for development
+    cors: true,
     headers: {
       'Access-Control-Allow-Origin': '*',
-    }
+    },
   },
   build: {
     outDir: 'dist',
     sourcemap: true,
+    chunkSizeWarningLimit: 1000, // Increase limit to suppress warnings (optional)
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'react-vendor';
+            if (id.includes('firebase')) return 'firebase-vendor'; // Example for Firebase
+            return 'vendor';
+          }
+        },
+      },
+    },
   },
-})
+  base: '/Student-Attendance-System/',
+});
